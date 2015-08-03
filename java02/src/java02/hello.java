5529f8cb99771db77595729f993b8a185b356811
GoofyJM <as88888as@gmail.com>
2015-08-03 15:33:36 +0800

    funnyguy

---
+++ java02/src/java02/hello.java
@@ -0,0 +1,29 @@
+package java02;
+
+import java.util.Scanner;
+
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner scn = new Scanner(System.in);
+	System.out.println("hi 請問你是");
+	String name = scn.next();
+	System.out.println("HI,"+name+"你好歡迎來到亞大");
+	System.out.println(name+"很高興認識你,請穩你是哪一所高中畢業的");
+	String pp= scn.next();
+	System.out.println(pp+",真是一所爛學校");
+	System.out.println("那你現在讀哪裡");
+	String fun= scn.next();
+	System.out.println("你讀"+fun+"喔哈哈哈哈");
+	System.out.println("祝你大學愉快");
+	
+	
+	
+	}
+	
+	
+	
+
+}
