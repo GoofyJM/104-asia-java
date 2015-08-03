9105bea7dc06f2b853e6a5d4c5ed813bb710bbc9
GoofyJM <as88888as@gmail.com>
2015-08-03 14:01:35 +0800

    課後練習2015-8-03

---
+++ JAVA01/src/java03.java
@@ -0,0 +1,26 @@
+
+public class java03 {
+	
+		public static void main(String[] args) {
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡´¡´¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡´¡´¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡´¡´¡³¡´¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡´¡´¡´¡´¡³¡³¡´¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡´¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡´¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡´¡³¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡³¡³¡³¡´¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡´¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡´¡³¡³¡³¡³");
+			
+			
+			
+			
+		}
+
+		}
---
+++ JAVA01/src/java04.java
@@ -0,0 +1,34 @@
+
+public class java04 {
+	
+		public static void main(String[] args) {
+		System.out.println("    ╭－－－－－－－－－－－－╮  ");
+		System.out.println("      | 　　                                 　      　|   ");
+		System.out.println("      | 　　                                 　      　|   ");
+		System.out.println(" ╞=====☉==☉=======╡");
+		System.out.println("      | 　　                                 　      　|   ");
+		System.out.println("      | 　　                                 　      　|   ");
+		System.out.println("      | 　　               oo       |   ");
+		System.out.println("      | 　　             ╰-╯                     |   ");
+		System.out.println("╭-┥                                                   ┝ -╮　  ");
+		System.out.println("|╭┥　　                                          ┝ ╮| ");
+		System.out.println("||▉▉▉▉▉▉▉▉▉▉▉▉▉▉ ||");
+		System.out.println("||▉▉▉▉▉▉▉▉▉▉▉▉▉▉ ||");
+		System.out.println("||▉▉▉▉▉▉▉▉▉▉▉▉▉▉ ||");
+		System.out.println("╰╯▉▉▉▉▉▉▉▉▉▉▉▉▉▉╰╯");
+		System.out.println("        ▉▉▉▉▉▉▉▉▉▉▉▉▉▉                                 ");
+		System.out.println("        ▉▉▉▉▉▉▉▉▉▉▉▉▉▉                                 ");
+		System.out.println("         　     　 |    |           |    |    ");
+		System.out.println("         　     　 |    |           |    |    ");
+		System.out.println("                     ╭╨╨   ╨╨╮");
+		System.out.println("                     ╰-╯      ╰-╯        ");
+		System.out.println("      ");
+		System.out.println("      ");
+		System.out.println("      ");
+			
+			
+			
+			
+			
+}
+}
