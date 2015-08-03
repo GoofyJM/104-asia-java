fc87c3b8b3c37d618d4ca75545ba4560b4c93cf2
GoofyJM <as88888as@gmail.com>
2015-08-03 16:23:57 +0800

    circle finish

---
+++ java02/src/java02/circle.java
@@ -0,0 +1,20 @@
+package java02;
+import java.util.Scanner;
+public class circle {
+		public static void main(String[] args) {
+		double pi=3.14;
+		Scanner scn = new Scanner(System.in);
+		System.out.println("算個圓面積吧，給我圓的半徑");
+		float cc = scn.nextFloat();
+		System.out.println("半徑是"+cc);
+		double answer = cc*cc*pi;
+		System.out.println("那你所求的圓形面積是"+answer);
+		
+		
+		
+		
+}
+	
+	
+	
+}
