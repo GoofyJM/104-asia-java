34bfb86584dd8c8ab269e2a5f48bb9397f9e74df
GoofyJM <as88888as@gmail.com>
2015-08-04 15:28:51 +0800

    finish

---
+++ java03/src/mangod01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class mangod01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入身高");
+		float tall = scn.nextFloat();
+		System.out.print("請輸入體重");
+		float weight = scn.nextFloat();
+		if (tall>165&&weight<45){
+			System.out.print("就交往吧");		
+		}else{System.out.print("可以不要嗎");
+		
+		
+		
+		
+		
+		
+	}
+
+}
+}
