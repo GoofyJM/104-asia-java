30f3d0fc3a8057c423fb5888b2bc8fd3027b8957
GoofyJM <as88888as@gmail.com>
2015-08-04 14:33:26 +0800

    finish2

--- java03/src/bigsmall.java
+++ java03/src/bigsmall.java
@@ -9,9 +9,9 @@ public class bigsmall {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		System.out.println("請輸入a值");
+		System.out.print("請輸入a值");
 		float big = scn.nextFloat();
-		System.out.println("請輸入b值");
+		System.out.print("請輸入b值");
 		float small = scn.nextFloat();
 		if (big>small)
 		{System.out.println(big+">"+small);
