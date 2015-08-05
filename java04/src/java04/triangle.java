f17363516360b7660168c1d4579e9920d5e014cb
GoofyJM <as88888as@gmail.com>
2015-08-05 10:31:43 +0800

    okkkkkkkkk

---
+++ java04/src/java04/triangle.java
@@ -0,0 +1,10 @@
+package java04;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java04/src/java04/vegetable01.java
@@ -0,0 +1,10 @@
+package java04;
+
+public class vegetable01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
