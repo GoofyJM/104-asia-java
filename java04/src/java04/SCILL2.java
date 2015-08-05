e1e89e8aaf1a556974fdf494ad2b069733c8e236
GoofyJM <as88888as@gmail.com>
2015-08-05 16:10:17 +0800

    ????

---
+++ java04/src/java04/SCILL2.java
@@ -0,0 +1,21 @@
+package java04;
+
+import java.util.Scanner;
+
+public class SCILL2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("請輸入英文字母轉換大小寫");
+		String a = scn.next();
+		char[] b  = a.toCharArray();
+		if((b[0]-'a')>=0 &&(b[0]-'a')<26){
+		b[0] =(char) (65+(b[0]-'a')); 
+		System.out.print("這是小寫");
+		}else{b[0] =(char) (32+(b[0])); 
+		System.out.print("這是大寫");
+		}		
+}
+}
