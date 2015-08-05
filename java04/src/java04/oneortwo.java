b434bcbca29dca3dc91a28079096516d7d226db6
GoofyJM <as88888as@gmail.com>
2015-08-05 14:38:01 +0800

    gogogogogog

---
+++ java04/src/java04/oneortwo.java
@@ -0,0 +1,24 @@
+package java04;
+import java.util.Scanner;
+public class oneortwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("叫块タ俱计");
+		int number = scn.nextInt();
+		if(number%2==1){
+		System.out.print("硂琌计");
+		}else{
+		System.out.print("硂琌案计");
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
