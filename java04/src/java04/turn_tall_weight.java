bb539ac433fad122309adf2945bf158a304c8740
GoofyJM <as88888as@gmail.com>
2015-08-05 14:53:13 +0800

    turn_tall_weight

---
+++ java04/src/java04/turn_tall_weight.java
@@ -0,0 +1,28 @@
+package java04;
+import java.util.Scanner;
+public class turn_tall_weight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入身高");
+		float tall = scn.nextFloat();
+		System.out.println("請輸入體重");
+		float weight = scn.nextFloat();
+		double talll = tall/2.45;
+		double weightt = weight/0.454;
+		System.out.println("身高是"+talll+"吋");
+		System.out.println("體重是"+weightt+"磅");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
