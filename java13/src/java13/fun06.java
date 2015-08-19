89ff9dfe8a6e457bc5edb1e244ff761aa3047f16
GoofyJM <as88888as@gmail.com>
2015-08-19 16:05:25 +0800

    2015/8/19

---
+++ java13/src/java13/fun06.java
@@ -0,0 +1,36 @@
+package java13;
+import java.util.Scanner;
+public class fun06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("½Ð¿é¤J¤@¦ê¦r");
+		String str = scn.nextLine();
+		System.out.println(fac(str));
+		
+	}
+		public static String fac(String str){
+			
+		
+		char a[] =str.toCharArray(); 
+		int i =a.length;
+		for(int n =0;n<i;n++){
+			if(a[n]!=32&&a[n]<=57&&a[n]>=48){
+				System.out.print(a[n]);
+			}
+			
+		}
+		return "";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
